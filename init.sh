cfe87268f9b1f2ec5363124aa3cd3699d39d2357
ksripathi <kammari.sripathi@gmail.com>
2017-06-08 21:46:40 +0530

    added index.org

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 if [ -d literate-tools ]; then
     echo "literate-tools already present"
-    (cd literate-tools; git pull)
+    (cd literate-tools; git checkout readtheorg; git pull readtheorg)
 else
     git clone https://github.com/vlead/literate-tools.git
 fi
