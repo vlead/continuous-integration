a4c2d940b74514f09d95353a6cd48c9a5dd1dc04
ksripathi <kammari.sripathi@gmail.com>
2017-06-08 21:23:16 +0530

    refactored requirements

--- buildScript.sh
+++
@@ -1,8 +0,0 @@
-!# /bin/bash
-cd /home/vlead/ .jenkins/workspace/lds-test
-make
-mkdir lds_docs
-cp -r lds-test/build/docs lds_docs
-rm lds-test
-rsync -avz --progress build/docs/ root@10.4.12.63:/var/www/lds-test
-rm lds_docs
