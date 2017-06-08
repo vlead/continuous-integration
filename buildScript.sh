24ad3475ec3c9ad941a90b8965e7e483db6d3d2b
savarmehrotra <savarmehrotra@gmail.com>
2017-06-08 16:13:20 +0530

    Create buildScript.sh
    shell script for build operation on Jenkins CI tool

--- buildScript.sh
+++ buildScript.sh
@@ -1,5 +1,8 @@
 !# /bin/bash
+cd /home/vlead/ .jenkins/workspace/lds-test
+make
 mkdir lds-test
 mkdir lds_docs
 cp -r lds-test/build/docs lds_docs
 rm lds-test
+rsync -avz --progress build/docs/ root@10.4.12.63:/var/www/lds-test
